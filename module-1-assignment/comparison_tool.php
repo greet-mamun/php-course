3c62aabc65bf606218d05a409815b2205021176f
Al Mamun <mamunhpath@hotmail.com>
2023-09-11 22:34:00 +0600

    Task 6 completed

---
+++ module-1-assignment/comparison_tool.php
@@ -0,0 +1,24 @@
+<?php
+
+    $number1 = $_POST['number1'];
+    $number2 = $_POST['number2'];
+    $submit = $_POST['submit'];
+    $is_larger = '';
+    if(isset($submit)){
+       $is_larger = $number1 > $number2 ? "$number1 is larger." : "$number2 is larger.";
+    };
+
+    echo
+    "<h2>Task 6: Comparison Tool</h2>
+
+    <form action='' method='post'>
+        <label for='number1'>Number 1:</label><br>
+        <input type='number' id='number1' name='number1' value='0'><br>
+        <label for='number2'>Number 2:</label><br>
+        <input type='number' id='number2' name='number2' value='0'><br>
+        <input type='submit' name='submit' value='Submit'>
+    </form>";
+?>
+    <h3><?php echo $is_larger; ?></h3>
+
+
