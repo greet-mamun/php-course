c8de3e86644a747c0ae0438c7d93cd046c96c3aa
Al Mamun <mamunhpath@hotmail.com>
2023-09-11 21:49:49 +0600

    task two completed

---
+++ module-1-assignment/temperature_converter.php
@@ -0,0 +1,35 @@
+<?php
+
+    $temperature = $_POST['temperature'];
+    $direction_value = $_POST['select_direction'];
+    $submit = $_POST['submit'];
+    $celcius = 0;
+    $farenheit = 0;
+    $message = '';
+
+    if(isset($submit)){
+        if($direction_value == 'celcius'){
+            $celcius = ($temperature - 32) * 5/9;
+            $message =  "Temperature is $celcius degree celcius.";
+        } elseif ($direction_value == 'farenheit'){
+            $farenheit = ($temperature * 9/5) + 32;
+            $message = "Temperature is $farenheit degree farenheit.";
+        }
+    }
+
+    echo
+    "<h2>Temperature Converter</h2>
+
+    <form action='' method='post'>
+        <label for='temperature'>Temperature Value:</label><br>
+        <input type='number' id='temperature' name='temperature' value='0'><br>
+        <select name='select_direction'>
+            <option value='celcius'>Celcius</option>
+            <option value='farenheit'>Farenheit</option>
+        </select><br>
+        <input type='submit' name='submit' value='Submit'>
+    </form>";
+?>
+    <h3><?php echo $message; ?></h3>
+
+
