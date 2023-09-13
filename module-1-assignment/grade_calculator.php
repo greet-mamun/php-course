000bcfce47476741c9fd42a5ce0676d14d7c4f7a
Al Mamun <mamunhpath@hotmail.com>
2023-09-13 22:57:25 +0600

    task 3 completed

---
+++ module-1-assignment/grade_calculator.php
@@ -0,0 +1,34 @@
+<?php
+
+    $score1 = $_POST['score1'];
+    $score2 = $_POST['score2'];
+    $score3 = $_POST['score3'];
+    $submit = $_POST['submit'];
+    $result = '';
+    if(isset($submit)){
+       $avarage = ($score1 + $score2 + $score3) / 3;
+       $result = match (true) {
+            $avarage >= 80 => 'Your result is A',
+            $avarage >= 60 => 'Your result is B',
+            $avarage >= 50 => 'Your result is C',
+            $avarage >= 33 => 'Your result is D',
+            default => 'Your result is F',
+       };
+    };
+
+    echo
+    "<h2>Task 3: Grade Calculator</h2>
+
+    <form action='' method='post'>
+        <label for='score1'>Score 1:</label><br>
+        <input type='number' id='score1' name='score1' value='0'><br>
+        <label for='score2'>Score 2:</label><br>
+        <input type='number' id='score2' name='score2' value='0'><br>
+        <label for='score3'>Score 2:</label><br>
+        <input type='number' id='score3' name='score3' value='0'><br>
+        <input type='submit' name='submit' value='Submit'>
+    </form>";
+?>
+    <h3><?php echo $result; ?></h3>
+
+
